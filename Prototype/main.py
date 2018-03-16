ed5d96a2079efc9019ba52647a6ba29438d17f52
Chase Puskar <chasepuskar@gmail.com>
2018-03-16 17:33:14 -0600

    setting up prototype folder

---
+++ Prototype/main.py
@@ -0,0 +1,12 @@
+##################################################################
+#Purpose:
+#   - To create a prototype that displays the concept of operation
+#for the software during the pressure stages
+#   - Use LED's to indicate what valve/stage/time it is on
+#Created: 3/13/2018
+#Modified: -/-
+#Miura 2: Prototype (proto.py)
+#Project: Miura 2
+##################################################################
+#   -- Imports & Housing Keeping --
+import RPi.GPIO as GPIO
---
+++ Prototype/mdrive_sample.py
@@ -0,0 +1,16 @@
+##################################################################
+#Purpose:
+#   - To create a sample code that runs the motor on prompter's command
+#   - Be able to go either direction and various speeds
+#Created: 3/13/2018
+#Modified: -/-
+#Miura 2: Motor Driver Sample Code (mdrive_sample.py)
+#Project: Miura 2
+##################################################################
+#   -- Key Information --
+# Add pins for the motor
+
+#   -- Imports & Housing Keeping --
+
+		
+

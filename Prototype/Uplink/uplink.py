d5323490e4ea0d99adeab2d9b2a621f2f23a1466
Srikanth <srikanth817@gmail.com>
2018-03-16 20:19:19 -0600

    Updated Protoype Messager

--- Prototype/Uplink/uplink.py
+++ Prototype/Uplink/uplink.py
@@ -1,30 +1,34 @@
 # Sample Uplink Code
 import serial
 
-def uplink():
-	# sets up serial object for use
-	ser = serial.Serial(port='/dev/',
-						baudrate=4800,
-						parity=serial.PARITY_NONE,
-						stopbits=serial.STOPBITS_ONE,
-						bytesize=serial.EIGHTBITS,
-						writeTimeout=None,
-						timeout=0)
+#def uplink():
+# sets up serial object for use
+ser = serial.Serial(port='/dev/tty.usbserial',
+					 baudrate=4800,
+					# parity=serial.PARITY_NONE,
+					# stopbits=serial.STOPBITS_ONE,
+					# bytesize=serial.EIGHTBITS,
+					# writeTimeout=None,
+					# timeout=0)
+					)
 
-	# sets list of valid commands
-	commands = ['xAA','xAB','xAC','xAC','xAD']
-	commands_usage = ['Ping Pi','Extend Structure','Retract Structure','Infinite Loop']
+# sets list of valid commands
+commands = ['xAA','xAB','xAC','xAC','xAD']
+commands_usage = ['Ping Pi','Extend Structure','Retract Structure','Infinite Loop']
 
-	# prompts user for valid command
-	print('Commands:\n')
-	for i in range(commands.len()):
-		print(commands[i] + ': ' + commands_usage + '\n')
-	print('\n')
+# prompts user for valid command
 
-	cmd = input('Enter Command: ')
-
-	while not any(cmd in command for command in commands):
-		cmd = input('Invalid Command\n\n Enter Command: ')
-
+print('Commands:\n')
+# for i in range(len(commands)):
+# 	print(commands[i] + ': ' + commands_usage + '\n')
+# print('\n')
+while True:
+	#cmd =bytes(input('Enter Command: '), 'utf-8')
+	cmd = (b'1')
 	ser.write(cmd)
-	ser.close
+
+# while not any(cmd in command for command in commands):
+# 	cmd = input('Invalid Command\n\n Enter Command: ')
+
+ser.write(cmd)
+ser.close

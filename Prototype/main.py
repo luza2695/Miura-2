15ba24dd117bec586e0cfa7b5625284f719a817b
Chase Puskar <chasepuskar@engr2-27-167-dhcp.int.colorado.edu>
2018-03-23 12:17:51 -0600

    fixed up headers and organization

--- Prototype/main.py
+++ Prototype/main.py
@@ -1,28 +1,28 @@
 ##################################################################
-#Purpose:
-#   - To create a prototype that displays the concept of operation
-#for the software during the pressure stages
-#   - Use LED's to indicate what valve/stage/time it is on
-#Created: 3/13/2018
-#Modified: -/-
-#Miura 2: Prototype (proto.py)
-#Project: Miura 2
+# Purpose:
+#   - Demonstrate successful uplink and execution of commands
+# Created: 3/13/2018
+# Modified: 3/21/2018
+#
+# Project: Miura 2 (Prototype)
 ##################################################################
-#   -- Imports & Housing Keeping --
 
-# Import code shared between threads
+# imports
 import serial
 import uplink
 
+# sets current pi usb port
 current_port = "/dev/ttyUSB0"
+
 # Create serial object
-ground = serial.Serial(port=current_port, #user input
-			baudrate=4800, #user input
+ground = serial.Serial(port=current_port,
+			baudrate=4800,
 			parity=serial.PARITY_NONE,
 			stopbits=serial.STOPBITS_ONE,
 			bytesize=serial.EIGHTBITS,
 			timeout=1)
 
+# intializes loop to detect uplink
 while True:
     uplink.main(ground)
 

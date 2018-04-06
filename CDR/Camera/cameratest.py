91ca6cb023c80d54c0d81a7ac7d54ffdd115f72f
Chase Puskar <chasepuskar@gmail.com>
2018-04-06 15:43:27 -0600

    organized some folders

--- CDR/Camera/cameratest.py
+++ CDR/Camera/cameratest.py
@@ -5,5 +5,6 @@ start = time.time()
 
 os.system('fswebcam -i 0 -d /dev/video0 -r 1024x768 -S 10 test1.jpg')
 
+os.system('fswebcam -i 0 -d /dev/video1 -r 1024x768 -S 10 test2.jpg')
 
 print(time.time()-start)

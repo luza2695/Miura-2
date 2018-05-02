6601ac0887aeaf5a5545fb3edba728b691f6273e
Chase Puskar <chasepuskar@engr2-2-91-94-dhcp.int.colorado.edu>
2018-05-02 16:45:05 -0600

    indents

--- CDR/Sensors/test.py
+++ CDR/Sensors/test.py
@@ -25,7 +25,7 @@ def read_pressure():
     # MPL3115A2 address, 0x60(96)
     # Select control register, 0x26(38)
     #		0x39(57)	Active mode, OSR = 128, Barometer mode
-    bus.write_byte_data(0x60, 0x26, 0x39)
+	bus.write_byte_data(0x60, 0x26, 0x39)
     # MPL3115A2 address, 0x60(96)
 	# Read data back from 0x00(00), 4 bytes
 	# status, pres MSB1, pres MSB, pres LSB

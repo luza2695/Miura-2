f0e817af48323ee17b9440ac01e7a33aae164a94
Chase Puskar <chasepuskar@gmail.com>
2018-05-02 17:24:46 -0600

    Added downlink to utility thread

--- FInal/utility.py
+++ FInal/utility.py
@@ -1,26 +1,28 @@
 # imports
 import serial
 import time
-#import uplink
+import testDownlink
 
 # sets current pi usb port
-current_port = "/dev/ttyUSB0"
+#current_port = "/dev/ttyUSB0"
 
 # Create serial object
-ground = serial.Serial(port=current_port,
-                       baudrate=4800,
-                       parity=serial.PARITY_NONE,
-                       stopbits=serial.STOPBITS_ONE,
-                       bytesize=serial.EIGHTBITS,
-                       timeout=1)
-
+#ground = serial.Serial(port=current_port,
+#                       baudrate=4800,
+#                       parity=serial.PARITY_NONE,
+#                       stopbits=serial.STOPBITS_ONE,
+#                       bytesize=serial.EIGHTBITS,
+#                       timeout=1)
+#
 # downlinks given pakcet
-def downlink(packet):
-    ground.write(packet)
+#def downlink(packet):
+#    ground.write(packet)
+
+
 
 def main():
 	for i in range(0,4):
-		downlink(bytes(str(i),'utf-8'))
+		testDownlink.downlink(bytes(str(i),'utf-8'))
 	#time.sleep(1)
 	#downlink(bytes('b','utf-8'))
 	#time.sleep(1)

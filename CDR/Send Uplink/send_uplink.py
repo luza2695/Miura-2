627a5c5db27cfb76e94eafc6435e6f6b9c73f9be
Chase Puskar <chasepuskar@engr2-2-90-99-dhcp.int.colorado.edu>
2018-05-24 11:42:45 -0600

    stupid folders

--- CDR/Send Uplink/send_uplink.py
+++ CDR/Send Uplink/send_uplink.py
@@ -7,7 +7,7 @@ from sys import platform
 if platform == "linux" or platform == "linux2":
     current_port = "/dev/ttyUSB0"
 elif platform == "darwin":
-    current_port = "/dev/tty.KeySerial1"
+    current_port = "/dev/tty.USA19H142P1.1"
 
 # sets up serial object for use
 ser = serial.Serial(port=current_port,
@@ -23,21 +23,21 @@ commands_usage = ['Ping Pi','Demo Motor','Camera Demo']
 
 # prompts user for valid command
 
-# print('\nCommands:\r')
-# for i in range(len(commands)):
-# 	print('\t' + commands[i] + ': ' + commands_usage[i] + '\r')
+print('\nCommands:\r')
+for i in range(len(commands)):
+	print('\t' + commands[i] + ': ' + commands_usage[i] + '\r')
 
 
 
-# cmd_str = input('\nEnter Command: ')
-# if not any(cmd_str in command for command in commands) and False:
-# 	print('Invalid Command')
-# else: 
-# 	cmd = int(cmd_str,16)
+cmd_str = input('\nEnter Command: ')
+if not any(cmd_str in command for command in commands) and False:
+	print('Invalid Command')
+else: 
+	cmd = int(cmd_str,16)
 
-# 	cmd = bytes([cmd])#, 'utf-8')
+	cmd = bytes([cmd])#, 'utf-8')
 		
-# 	ser.write(cmd)
+	ser.write(cmd)
 	
 complete = False
 

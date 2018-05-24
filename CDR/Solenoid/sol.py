a4cfb55f78a7b6735c3e816a41ef191d79f6de9e
Chase Puskar <chasepuskar@engr2-2-90-99-dhcp.int.colorado.edu>
2018-05-24 10:54:28 -0600

    added solenoid code

---
+++ CDR/Solenoid/sol.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+solenoid_pin = 18
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(solenoid_pin, GPIO.OUT)
+
+def openSolenoid():
+	GPIO.output(solenoid_pin, True)
+
+def closeSolenoid():
+	GPIO.output(solenoid_pin, False)
+
+def alternate():
+	while True:
+		openSolenoid()
+		time.sleep(1)
+		closeSolenoid()
+		time.sleep(1)

cdb299f95782973a1bd12941ee44b0aea917937b
Chase Puskar <chasepuskar@engr2-2-90-99-dhcp.int.colorado.edu>
2018-05-24 11:37:53 -0600

    fixed send uplink and added to final

---
+++ Final/Send Uplink/send_uplink.py
@@ -0,0 +1,64 @@
+# Sample Uplink Code
+import serial
+import time
+from sys import platform
+
+
+if platform == "linux" or platform == "linux2":
+    current_port = "/dev/ttyUSB0"
+elif platform == "darwin":
+    current_port = "/dev/tty.USA19H142P1.1"
+
+# sets up serial object for use
+ser = serial.Serial(port=current_port,
+                    baudrate=4800,
+                    parity=serial.PARITY_NONE,
+                    stopbits=serial.STOPBITS_ONE,
+                    bytesize=serial.EIGHTBITS,
+                    timeout=1)
+
+# sets list of valid commands
+commands = ['x01','x02','x0C','x04','x05']
+commands_usage = ['Ping Pi','Demo Motor','Camera Demo','Open Solenoid','Close Solenoid']
+
+# prompts user for valid command
+
+print('\nCommands:\r')
+for i in range(len(commands)):
+	print('\t' + commands[i] + ': ' + commands_usage[i] + '\r')
+
+
+
+cmd_str = input('\nEnter Command: ')
+if not any(cmd_str in command for command in commands) and False:
+	print('Invalid Command')
+else: 
+	cmd = int(cmd_str,16)
+
+	cmd = bytes([cmd])#, 'utf-8')
+		
+	ser.write(cmd)
+	
+complete = False
+
+while (not complete):
+	time.sleep(0.5)
+	print (ser.inWaiting())
+
+	cmd_str = input('\nEnter Command: ')
+	if not any(cmd_str in command for command in commands) and False:
+		print('Invalid Command')
+	else: 
+		cmd = int(cmd_str,16)
+
+		cmd = bytes([cmd])#, 'utf-8')
+		
+		ser.write(cmd)
+	while ser.inWaiting():
+		
+		#print("",end='')
+		cmd = ser.read()
+            #packet = hex(int.from_bytes((cmd),byteorder = 'big'))
+		packet = cmd.decode("utf-8")
+		print(packet)
+		#complete = True

87c2849906d1cadbcf098d151bac5a64d23c5d80
Chase Puskar <chasepuskar@Chases-MacBook-Pro.local>
2018-05-29 10:28:31 -0600

    cleaned up uplink.py

--- Final/uplink.py
+++ Final/uplink.py
@@ -2,14 +2,11 @@
 #Purpose:
 #   - Accept uplink commands
 #Created: 3/13/2018
-#Modified: -/-
+#Modified: 5/29/2018
 #Miura 2: Uplink Code (uplink.py)
 #Project: Miura 2
 ##################################################################
-#   -- Key Information --
-#!/usr/bin/python
-#   -- Imports & Housing Keeping --
-import subprocess
+
 #import RPi.GPIO as GPIO
 import time
 import serial
@@ -37,7 +34,6 @@ ground = serial.Serial(port=current_port,
             bytesize=serial.EIGHTBITS,
             timeout=1)
 
-
 #LED ping when called
 #def led():
 #    GPIO.output(led_pin,GPIO.HIGH)
@@ -55,28 +51,27 @@ def main(ground):
             if cmd == b"\x01": # ping pi
                 cmdTime = time.asctime(time.localtime(time.time()))
                 ground.write(bytes(5))
-                #print(bytes(5))
-                #print("Command Recieved :")#, cmdTime)
+                print("Command Recieved :", cmdTime)
                 pass
             elif cmd == b"\x02": # demo motor
                 print("Begin Motor Command")
                 StepperTest.main()
-            elif cmd == b"\x03":
+            elif cmd == b"\x03": # open pressurization valve
                 print("Opening Pressurize Valve")
                 solenoid.openPressurize()
-            elif cmd == b"\x04":
+            elif cmd == b"\x04": # close pressurization valve
                 print("Closing Pressurize Valve")
                 solenoid.closePressurize()
-            elif cmd == b"\x05":
+            elif cmd == b"\x05": # open exhaust valve
                 print("Opening Exhaust Valve")
                 solenoid.openExhaust()
-            elif cmd == b"\x06":
+            elif cmd == b"\x06": # close exhaust valve
                 print("Closing Exhaust Valve")
                 solenoid.closeExhaust()
-            elif cmd == b"\x07":
+            elif cmd == b"\x07": # burp exhaust valve
                 print("Burping Exhaust Valve")
                 solenoid.burp()
-            elif cmd == b"\x0C":
+            elif cmd == b"\x0C": # take picture
                 print("Begin Camera Command")
                 #cameratest.main()
                 cmd = bytes('Nice picture!', 'utf-8')
@@ -84,3 +79,7 @@ def main(ground):
             else:
                 print("invalid command")
         time.sleep(1)
+
+# intializes loop to detect uplink
+while True:
+    uplink.main(ground)

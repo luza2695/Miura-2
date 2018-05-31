bdf820478c1151c5b2c7fc933918e4d80a32dd72
Chase Puskar <chasepuskar@Chases-MacBook-Pro.local>
2018-05-31 12:52:22 -0600

    uplink

--- Final/send_uplink.py
+++ Final/send_uplink.py
@@ -28,35 +28,35 @@ ser = serial.Serial(port=current_port,
 # commands_usage = ['Ping Pi','Demo Motor','Open Pressurize','Close Pressurize','Open Exhaust','Close Exhaust','Burp Exhaust','Camera Demo']
 
 commands = {
-	'\\x01 \\x01':'Ping Pi',
-	'\\x01 \\x02':'Manual Mode',
-	'\\x01 \\x03':'Automation Mode',
-	'\\x01 \\x04':'Retract Motor',
-	'\\x01 \\x05':'Reboot Pi',
-	'\\x01 \\x06':'Take Video',
-	'\\x02 \\x01':'Start Cycle',
-	'\\x02 \\x02':'Finish Retraction',
-	'\\x02 \\x03':'Finish Inflation',
-	'\\x03 \\x01':'Open Solenoid Valve 1',
-	'\\x03 \\x02':'Close Solenoid Valve 1',
-	'\\x03 \\x03':'Open Solenoid Valve 2',
-	'\\x03 \\x04':'Close Solenoid Valve 2',
-	'\\x03 \\x05':'Open Exhaust Value',
-	'\\x03 \\x06':'Close Exhaust Valve',
-	'\\x04 \\x01':'Disable Valve 1',
-	'\\x04 \\x02':'Enable Valve 1',
-	'\\x04 \\x03':'Disable Valve 2',
-	'\\x04 \\x04':'Enable Valve 2',
-	'\\x05 \\x01':'Turn On Solenoid Heaters',
-	'\\x05 \\x02':'Turn Off Solenoid Heaters',
-	'\\x05 \\x03':'Turn On Payload Heaters',
-	'\\x05 \\x04':'Turn Off Payload Heaters'
+	'\x01 \x01':'\\x01 \\x01 : Ping Pi',
+	'\x01 \x02':'\\x01 \\x02 : Manual Mode',
+	'\x01 \x03':'\\x01 \\x03 : Automation Mode',
+	'\x01 \x04':'\\x01 \\x04 : Retract Motor',
+	'\x01 \x05':'\\x01 \\x05 : Reboot Pi',
+	'\x01 \x06':'\\x01 \\x06 : Take Video',
+	'\x02 \x01':'\\x02 \\x01 : Start Cycle',
+	'\x02 \x02':'\\x02 \\x02 : Finish Retraction',
+	'\x02 \x03':'\\x02 \\x03 : Finish Inflation',
+	'\x03 \x01':'\\x03 \\x01 : Open Solenoid Valve 1',
+	'\x03 \x02':'\\x03 \\x02 : Close Solenoid Valve 1',
+	'\x03 \x03':'\\x03 \\x03 : Open Solenoid Valve 2',
+	'\x03 \x04':'\\x03 \\x04 : Close Solenoid Valve 2',
+	'\x03 \x05':'\\x03 \\x05 : Open Exhaust Value',
+	'\x03 \x06':'\\x03 \\x06 : Close Exhaust Valve',
+	'\x04 \x01':'\\x04 \\x01 : Disable Valve 1',
+	'\x04 \x02':'\\x04 \\x02 : Enable Valve 1',
+	'\x04 \x03':'\\x04 \\x03 : Disable Valve 2',
+	'\x04 \x04':'\\x04 \\x04 : Enable Valve 2',
+	'\x05 \x01':'\\x05 \\x01 : Turn On Solenoid Heaters',
+	'\x05 \x02':'\\x05 \\x02 : Turn Off Solenoid Heaters',
+	'\x05 \x03':'\\x05 \\x03 : Turn On Payload Heaters',
+	'\x05 \x04':'\\x05 \\x04 : Turn Off Payload Heaters'
 }
 
 # prompts user for valid command
 print('\nCommands:\r')
-for key, value in zip(commands.keys(), commands.values()):
-	print('\t' + key + ': ' + value + '\r')
+for value in commands.values():
+	print('\t' + value + '\r')
 	
 complete = False
 

c09879a1ca292c6399d89b0c325405ae583384d4
Chase Puskar <chasepuskar@Chases-MacBook-Pro.local>
2018-06-04 13:25:29 -0600

    indents

--- Final/uplink.py
+++ Final/uplink.py
@@ -77,7 +77,7 @@ def main(serial, downlink_queue, manual):
                 manual = False
             elif command == b'\x04': # retract motor
                 StepperTest.main()
-           	    downlink_queue.put(['MO','RE',0])
+                downlink_queue.put(['MO','RE',0])
 	       elif command == b'\x05': # take picture
                 #cameras.singlePic()
                 pass

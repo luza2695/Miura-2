ee55726156cde44128650ebe4f2cfcf856219345
Chase Puskar <chasepuskar@Chases-MacBook-Pro.local>
2018-06-05 15:14:33 -0600

    lights

--- Final/lights.py
+++ Final/lights.py
@@ -70,77 +70,77 @@ delay = 0.15
 #brightness: 0-31
 brightness = 15
 
-set_LED(0,25,25,25,brightness)
+#set_LED(0,25,25,25,brightness)
 
-# try:
-#     while True:
+try:
+    while True:
 
-#         for y in range (5):
+        for y in range (5):
 
-#             #center, white
-#             set_LED(0,25,25,25,brightness)
+            #center, white
+            set_LED(0,25,25,25,brightness)
 
-#             #inner ring, red
-#             for x in range (7):
-#                 set_LED(x+1,25,0,0,brightness)
+            #inner ring, red
+            for x in range (7):
+                set_LED(x+1,25,0,0,brightness)
 
-#             #outer ring, white
-#             for x in range (12):
-#                 set_LED(x+8,25,25,25,brightness)
+            #outer ring, white
+            for x in range (12):
+                set_LED(x+8,25,25,25,brightness)
 
-#             #Write to the LEDs and wait
-#             WriteLEDs()
-#             time.sleep(wait)
+            #Write to the LEDs and wait
+            WriteLEDs()
+            time.sleep(wait)
 
-#             #center, red
-#             set_LED(0,25,0,0,brightness)
+            #center, red
+            set_LED(0,25,0,0,brightness)
 
-#             #inner ring, white
-#             for x in range (7):
-#                 set_LED(x+1,25,25,25,brightness)
+            #inner ring, white
+            for x in range (7):
+                set_LED(x+1,25,25,25,brightness)
 
-#             #outer ring, red
-#             for x in range (12):
-#                 set_LED(x+8,25,0,0,brightness)
+            #outer ring, red
+            for x in range (12):
+                set_LED(x+8,25,0,0,brightness)
 
-#             #Write to the LEDs and wait                               
-#             WriteLEDs()
-#             time.sleep(wait)
+            #Write to the LEDs and wait                               
+            WriteLEDs()
+            time.sleep(wait)
 
-#         for y in range (5):
+        for y in range (5):
 
-#             #center, yellow
-#             set_LED(0,25,25,0,brightness)
+            #center, yellow
+            set_LED(0,25,25,0,brightness)
 
-#             #inner ring, blue
-#             for x in range (7):
-#                 set_LED(x+1,0,0,25,brightness)
+            #inner ring, blue
+            for x in range (7):
+                set_LED(x+1,0,0,25,brightness)
 
-#             #outer ring, yellow
-#             for x in range (12):
-#                 set_LED(x+8,25,25,0,brightness)
+            #outer ring, yellow
+            for x in range (12):
+                set_LED(x+8,25,25,0,brightness)
 
-#             #Write to the LEDs and wait
-#             WriteLEDs()
-#             time.sleep(wait)
+            #Write to the LEDs and wait
+            WriteLEDs()
+            time.sleep(wait)
 
-#             #center, blue
-#             set_LED(0,0,0,25,brightness)
+            #center, blue
+            set_LED(0,0,0,25,brightness)
 
-#             #inner ring, yellow
-#             for x in range (7):
-#                 set_LED(x+1,25,25,0,brightness)
+            #inner ring, yellow
+            for x in range (7):
+                set_LED(x+1,25,25,0,brightness)
 
-#             #outer ring, blue
-#             for x in range (12):
-#                 set_LED(x+8,0,0,25,brightness)
+            #outer ring, blue
+            for x in range (12):
+                set_LED(x+8,0,0,25,brightness)
 
-#             #Write to the LEDs and wait                           
-#             WriteLEDs()
-#             time.sleep(wait)
+            #Write to the LEDs and wait                           
+            WriteLEDs()
+            time.sleep(wait)
 
-# except KeyboardInterrupt:
-#     pass
+except KeyboardInterrupt:
+    pass
 
 
 

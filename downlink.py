8451ab77ba5e7e6a034f3f120ff5c79d53b6a203
Chase Puskar <chasepuskar@gmail.com>
2018-06-30 11:00:02 -0600

    added cycle tracking

--- downlink.py
+++ downlink.py
@@ -8,13 +8,13 @@ import serial
 import time
 from zlib import adler32
 
-def main(serial, downlink_queue, log_filename, stage):
+def main(serial, downlink_queue, log_filename, stage, current_cycle)x:
 	if not downlink_queue.empty():
 		message = downlink_queue.get() # gets message from downlink queue
 		sender, data_type, data = message[0], message[1], str(message[2]) # isolates into sender of message, data type, and the data
 		checksum = adler32(data.encode()) # calculates checksum
 		current_time = time.time() # gets current time
-		packet = '\x01CU MI2 %s %s %.2f %i %i\x02' % (sender, data_type, current_time, checksum, stage) + ' ' + data + '\x03\n'
+		packet = '\x01CU MI2 %s %s %.2f %i %i %i\x02' % (sender, data_type, current_time, checksum, current_cycle, stage) + ' ' + data + '\x03\n'
 		with open(log_filename, 'a') as log: # opens log file
 			log.write(packet) # writes to log file
 			serial.write(packet.encode()) # writes message to serial
--- main.py
+++ main.py
@@ -100,12 +100,11 @@ current_solenoid = 1
 current_cycle = 1
 stage, stage_start_time  = changeStage(1)
 
-print("Begin Cycles")
 # pressure check loop
 while running:
 	#Start the uplink/downlink
 	manual, stage, stage_start_time, solenoid_1_enabled, solenoid_2_enabled = uplink.main(serial, downlink_queue, data_directory, manual, stage, stage_start_time, solenoid_1_enabled, solenoid_2_enabled)
-	downlink.main(serial, downlink_queue, log_filename, stage)
+	downlink.main(serial, downlink_queue, log_filename, stage, current_cycle)
 
 	#Track the current time
 	current_time = time.time()
@@ -127,7 +126,6 @@ while running:
 			#cameras.videoCamera()
 
 			solenoid.openExhaust()
-
 
 			#conditionals ...
 			#	- after 4 hours into flight
